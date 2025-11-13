036c862dfaacf32848ddfc240b3124540b7a8ee2
Farrukh Ganiboyev <farrukhechos@gmail.com>
2025-11-12 23:12:18 -0800

    Lecture if elif

--- 10-lecture/index.py
+++ 10-lecture/index.py
@@ -2,9 +2,9 @@
 # son kiritiladi 3 kiritiladi fizz chiqsin agar 5 ga bolsinza bazz, agar 3 va 5 ga bolinsa fizzbazz
 number = int(input("Enter a number:"))
 
-if number % 3 ==0:
+if number % 3 ==0 and number % 5 ==0:
+    print("FizzBuzz")
+elif number % 3 ==0:
     print("Fizz")
 elif number % 5 ==0:
     print("Buzz")
-elif number % 3 ==0 and number % 5 ==0:
-    print("FizzBuzz")
