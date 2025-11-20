a912fd81ea40476fee7900c5abfea3fc5fe0002c
Farrukh Ganiboyev <farrukhechos@gmail.com>
2025-11-19 23:44:46 -0800

    New problems

--- 13-darsLOOPS/index.py
+++ 13-darsLOOPS/index.py
@@ -50,8 +50,75 @@ from unicodedata import digit
 # print(manfiy)
 
 # 7
-word = "My name is Farrukh and age 18"
+# word = "My name is Farrukh and age 18"
+#
+# for x in word:
+#     if x in "0123456789":
+#         print(x)
+# #
 
-for x in word:
-    if x in "0123456789":
-        print(x)
+
+
+
+
+
+# # While
+# parol_saved = 1111
+# pass_parol = int(input("Enter your parola number: "))
+#
+# while pass_parol != parol_saved:
+#     pass_parol = int(input("Enter your parola number: "))
+
+
+# A kesmadagi uzunlikka b kesmadai nechta sig'adi
+
+# a = int(input("Enter uzunlik: e.g 10 "))
+# b = int(input("Enter uzunlik: e.g 4 "))
+# count = 0
+# newResult = 0
+# while a >= b:
+#     count += 1
+#     newResult += b
+#     if newResult == a:
+#         print(count)
+
+
+    # while a >= b:
+    #     a -= b
+    #     count += 1
+    # print(count)
+
+    # n = int(input("Enter a number: "))
+
+
+    # ekranda 2ta qiymat kiritaman s va f s7 ga teng f 28
+
+# start = int(input("Enter a start number: "))
+# finish = int(input("Enter a finish number: "))
+#
+# for i in range(start, finish+1):
+#         print(i)
+
+# numbers = [1,2,390,34,8,9,123]
+# print(numbers)
+#
+# x = int(input("Enter a number: "))
+# check = False
+# for number in numbers:
+#     if number == x:
+#         check = True
+#         break
+#
+# if check:
+#     print(f"{x} bor")
+# else:
+#     print(f"{x} yo'q")
+
+# number = [1,2,3,4,5,6,7,8,-9,-2,-1]
+#
+# count = 0
+# for i in number:
+#     if i < 0:
+#         count += 1
+#
+# print("Manfiy raqamlar", count)
