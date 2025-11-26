4fdf503ed4de24b95300cb26c8d761b03498c5d2
Farrukh Ganiboyev <farrukhechos@gmail.com>
2025-11-25 22:10:24 -0800

    Today we have two lesson now we are solving porblems

---
+++ 15-darsMasala/index.py
@@ -0,0 +1,11 @@
+
+# 1:
+# fruits = ["apple", "banana", "orange", "peach", "strawberry", "cherry"]
+# longer = fruits[0]
+# for fruit in fruits:
+#     if len(fruit) > len(longer):
+#         longer = fruit
+#
+# print(longer)
+
+# 2:
